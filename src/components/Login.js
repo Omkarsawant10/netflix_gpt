f853a07bfb784c71c2cc26517e75e385966d249f
Omkarsawant10 <122078907+Omkarsawant10@users.noreply.github.com>
2024-01-22 22:52:25 +0530

    TMDB API data fetch and built main container

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,16 +3,16 @@ import { useRef, useState } from "react";
 import { checkValidData } from "../utils/validate";
 import { createUserWithEmailAndPassword ,signInWithEmailAndPassword,updateProfile} from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { AVTAR_LOGO } from "../utils/constants";
 
 const Login=()=>{
   const [isSignInForm,setisSignInForm]=useState(true);
 
   const [errorMessage,seterrorMessage]=useState(null);
 
-  const navigate=useNavigate();
+  
   const dispatch=useDispatch();
 
   const email=useRef(null);
@@ -35,10 +35,10 @@ const Login=()=>{
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-    console.log(user);
+    
 
     updateProfile(user, {
-      displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/12824231?v=4"
+      displayName: name.current.value, photoURL:AVTAR_LOGO,
     }).then(() => {
       // Profile updated!
       // ...
@@ -48,7 +48,7 @@ const Login=()=>{
         uid:uid,
         email:email,displayName:displayName,photoURL:photoURL
       }));
-      navigate("/browse")
+      
     }).catch((error) => {
       // An error occurred
       // ...
@@ -72,8 +72,8 @@ const Login=()=>{
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user);
-    navigate("/browse")
+    
+    
     // ...
   })
   .catch((error) => {
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const AVTAR_LOGO="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzU0NmQyMTVhMTVkZGY3NjljNjI1MmU3ZTJjOGVlOCIsInN1YiI6IjY1YWMwODQzODQ4ZWI5MDBhYzliMDY3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XkkXCN7MsKTlJ1IfjwgKf_RcRGQuHlpSmhidrCA4LUw'
+    }
+  };
